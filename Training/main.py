f234aa82d8048783438b05ee6066f7d3db018eac
zubalb <pendlumlive@gmail.com>
2026-05-14 17:32:06 +0300

    ready for netmiko

---
+++ Training/main.py
@@ -0,0 +1,31 @@
+from netmiko import ConnectHandler
+import getpass
+import json
+
+def get_device_type(vendor):
+    if vendor == "cisco":
+        return  "cisco_ios_telnet"
+    elif vendor == "huawei":
+        return "huawei_telnet"
+    else:
+        return "Неизвестный вендор"
+
+def get_command(vendor):
+    num_port = int(input("Введите последнюю цифру номера порта:"))
+    if vendor == "cisco":
+        return (f"interface GigabitEthernet0/{num_port}")
+    elif vendor == "huawei":
+        return (f"interface GigabitEthernet0/0/{num_port}")
+    else:
+        return "Неизвестный вендор:"
+try:
+    with open("devices.json", "r") as file:
+        data = json.load(file)
+    for device in data:
+            print(f"IP: {device['ip']} | Порт: {get_command(device['vendor'])} | Вендор: {get_device_type(device['vendor'])} | Пользователь: {device['username']}")
+except FileNotFoundError:
+    print("Error! Файл не найден!")
+except json.JSONDecodeError as e:
+    print(f"Неизвестная ошибка {e}")
+
+
