30b5fc1184940c0bb5e69a3e9eabe265ac0aa0d9
zubalb <pendlumlive@gmail.com>
2026-05-19 14:11:26 +0300

    changed config, ready for use for json files added check added time check

--- Training/main.py
+++ Training/main.py
@@ -1,7 +1,8 @@
 from netmiko import ConnectHandler
-from netmiko.exceptions import NetmikoTimeoutException, NetmikoAuthenticationException
+from netmiko.exceptions import NetmikoTimeoutException, NetmikoAuthenticationException, ReadTimeout
 import getpass
 import json
+import time
 
 def get_device_type(vendor):
     if vendor == "cisco":
@@ -9,18 +10,17 @@ def get_device_type(vendor):
     elif vendor == "huawei":
         return "huawei_telnet"
     else:
-        return "Неизвестный вендор"
+        raise ValueError(f"Неизвестный вендор: {vendor} ")
 
 def get_command(vendor):
     num_port = input("Укажите номер порта в формате (x/x):")
     if vendor == "cisco":
-        return (f"interface FastEthernet{num_port}")
+        return (f"interface GigabitEthernet{num_port}"), num_port
     elif vendor == "huawei":
-        return (f"interface GigabitEthernet{num_port}")
+        return (f"interface GigabitEthernet{num_port}"), num_port
     else:
-        return "Неизвестный вендор:"
-    
-def connect_and_reset(device, command):
+        return "Неизвестный вендор:", num_port
+def connect_and_reset(device, command, num_port):
     connection = ConnectHandler(
         device_type=get_device_type(device['vendor']),
         port=device['telnet_port'],
@@ -30,13 +30,22 @@ def connect_and_reset(device, command):
     )
     print(f"Подключился к: {device['ip']}")
     connection.send_config_set([command, "shutdown", "no shutdown"])
+    time.sleep(3)
+    IntStatus = connection.send_command(f"show interface GigabitEthernet{num_port} description")
+    print(f"Результат перезагрузки:\n{IntStatus}")
+    time.sleep(3)
+    MacTable = connection.send_command(f"show mac address-table interface GigabitEthernet{num_port}")
+    print(f"Таблица коммутации:\n{MacTable}")
     connection.disconnect()
 try:
     with open("devices.json", "r") as file:
         data = json.load(file)
     for device in data:
-            command = get_command(device['vendor'])
-            connect_and_reset(device, command)
+            command, num_port = get_command(device['vendor'])
+            start = time.time()
+            connect_and_reset(device, command, num_port)
+            end = time.time()
+            print(f"Время на выполнение с учётом timeout: {end-start:.2f} сек")
 except FileNotFoundError:
     print("Error! Файл не найден!")
 except json.JSONDecodeError as e:
@@ -45,3 +54,6 @@ except NetmikoTimeoutException as e:
     print(f"Таймаут подключения: {e}")
 except NetmikoAuthenticationException as e:
     print(f"Неверный логин или пароль {e}")
+except ReadTimeout as e:
+    print(f"Устройство не отвечает: {e}")
+    
